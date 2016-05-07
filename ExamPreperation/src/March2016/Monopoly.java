f2456830ec5cab82e2635a13e7f0b41e061b2b06
DeianH94 <deianh94@gmail.com>
2016-05-07 11:47:34 +0300

    Added solutions for some of the previous exams.

---
+++ ExamPreperation/src/March2016/Monopoly.java
@@ -0,0 +1,74 @@
+package March2016;
+
+import java.util.Scanner;
+
+public class Monopoly {
+    private static int moneyPerTurn = 0;
+    private static int money = 50;
+    private static int turn = 0;
+    private static int totalHotels = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        sc.nextLine();
+
+        String[][] board = new String[row][col];
+
+        for (int i = 0; i < board.length; i++) {
+            String[] rows = sc.nextLine().split("");
+            for (int j = 0; j < rows.length; j++) {
+                board[i][j] = rows[j];
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            String[] line = board[i];
+            if (i % 2 == 0) {
+                for (int j = 0; j < line.length; j++) {
+                    update((i + 1) * (j + 1), line[j]);
+                }
+            } else {
+                for (int j = line.length - 1; j >= 0; j--) {
+                    update((i + 1) * (j + 1), line[j]);
+                }
+            }
+        }
+
+        System.out.printf("Turns %d\n", turn);
+        System.out.printf("Money %d\n", money);
+    }
+
+    private static void update(int i, String s) {
+        switch (s) {
+            case "H":
+                turn++;
+                totalHotels++;
+                System.out.printf("Bought a hotel for %d. Total hotels: %d.\n", money, totalHotels);
+                money = 0;
+                moneyPerTurn += 10;
+                break;
+            case "J":
+                System.out.printf("Gone to jail at turn %d.\n", turn);
+                turn += 3;
+                money += 2 * moneyPerTurn;
+                break;
+            case "F":
+                turn++;
+                break;
+            case "S":
+                int moneyToSpend = i;
+                moneyToSpend = moneyToSpend > money ? money : moneyToSpend;
+                money -= moneyToSpend;
+                System.out.printf("Spent %d money at the shop.\n", moneyToSpend);
+                turn++;
+                if (money < 0) {
+                    money = 0;
+                }
+                break;
+        }
+
+        money += moneyPerTurn;
+    }
+}
